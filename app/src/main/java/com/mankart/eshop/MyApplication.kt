c489283985bdb4bec39fae03dc58eddf8c93715c
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-08-14 23:15:22 +0700

    favorite: split install manager

--- app/src/main/java/com/mankart/eshop/MyApplication.kt
+++ app/src/main/java/com/mankart/eshop/MyApplication.kt
@@ -1,7 +1,7 @@
 package com.mankart.eshop
 
-import android.app.Application
+import com.google.android.play.core.splitcompat.SplitCompatApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication: Application()
+class MyApplication: SplitCompatApplication()

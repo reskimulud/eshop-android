baf74fa46f58f2f984681ab98edcff5957c5ccca
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-08-16 10:05:41 +0700

    setting: navigation #10 #21

--- setting/src/main/java/com/mankart/eshop/setting/ui/SettingFragment.kt
+++ setting/src/main/java/com/mankart/eshop/setting/ui/SettingFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mankart.eshop.setting.BuildConfig
 import com.mankart.eshop.setting.databinding.FragmentSettingBinding
@@ -27,6 +28,17 @@ class SettingFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.tvVersionApp.text = "v${BuildConfig.VERSION_NAME}"
+
+        binding.setLanguage.setOnClickListener {
+            showUnderDevelopmentToast()
+        }
+        binding.setTheme.setOnClickListener {
+            showUnderDevelopmentToast()
+        }
+    }
+
+    private fun showUnderDevelopmentToast() {
+        Toast.makeText(requireActivity(), "Under Development", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {

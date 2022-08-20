eb364193076673629773e6642cb08d75145db6e0
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-08-20 23:39:29 +0700

    security: encrypt data store use AES algorithm #39

--- core/src/main/java/com/mankart/eshop/core/data/source/local/datastore/PreferenceDataStore.kt
+++ core/src/main/java/com/mankart/eshop/core/data/source/local/datastore/PreferenceDataStore.kt
@@ -1,39 +1,51 @@
 package com.mankart.eshop.core.data.source.local.datastore
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.mankart.eshop.core.utils.Constants.EMPTY_DATA_STORE
+import com.mankart.eshop.core.utils.SecurityUtils
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class PreferenceDataStore @Inject constructor(private val dataStore: DataStore<Preferences>) {
+class PreferenceDataStore @Inject constructor(
+    private val dataStore: DataStore<Preferences>,
+    private val security: SecurityUtils
+) {
+    private val jsonEncode: Json = Json { encodeDefaults = true }
 
-    fun getUserToken(): Flow<String> = dataStore.data.map { it[USER_ACCESS_TOKEN] ?: EMPTY_DATA_STORE }
+    // ======================================================================== //
+
+    fun getUserToken(): Flow<String> = dataStore.data.secureMap { it[USER_ACCESS_TOKEN] ?: EMPTY_DATA_STORE }
 
     suspend fun saveUserToken(token: String) {
-        dataStore.edit { preferences ->
-            preferences[USER_ACCESS_TOKEN] = token
+        dataStore.secureEdit(token) { preferences, encryptedToken ->
+            preferences[USER_ACCESS_TOKEN] = encryptedToken
         }
     }
 
-    fun getUserEmail(): Flow<String> = dataStore.data.map { it[USER_EMAIL] ?: EMPTY_DATA_STORE }
+    fun getUserEmail(): Flow<String> = dataStore.data.secureMap { it[USER_EMAIL] ?: EMPTY_DATA_STORE }
 
     suspend fun saveUserEmail(email: String) {
-        dataStore.edit { preferences ->
-            preferences[USER_EMAIL] = email
+        dataStore.secureEdit(email) { preferences, encryptedEmail ->
+            preferences[USER_EMAIL] = encryptedEmail
         }
     }
 
-    fun getUserName(): Flow<String> = dataStore.data.map { it[USER_NAME] ?: EMPTY_DATA_STORE }
+    fun getUserName(): Flow<String> = dataStore.data.secureMap { it[USER_NAME] ?: EMPTY_DATA_STORE }
 
     suspend fun saveUserName(name: String) {
-        dataStore.edit { preferences ->
-            preferences[USER_NAME] = name
+        dataStore.secureEdit(name) { preferences, encryptedName ->
+            preferences[USER_NAME] = encryptedName
         }
     }
 
@@ -42,6 +54,34 @@ class PreferenceDataStore @Inject constructor(private val dataStore: DataStore<P
             it.clear()
         }
     }
+
+    // method untuk mengenkripsi data store
+    private suspend inline fun <reified T> DataStore<Preferences>.secureEdit(
+        value: T,
+        crossinline editStore: (MutablePreferences, String) -> Unit
+    ) {
+        edit {
+            val encryptedValue = security.encryptData(Json.encodeToString(value))
+            editStore.invoke(it, encryptedValue)
+        }
+    }
+
+    // method untuk mendekripsi value data store
+    private inline fun <reified T> Flow<Preferences>.secureMap(
+        crossinline fetchValue: (value: Preferences) -> String,
+    ): Flow<T> =
+        map { preference ->
+            val value = fetchValue(preference)
+
+            if (value.isNotEmpty() && value != EMPTY_DATA_STORE) {
+                Timber.e("value: $value")
+                val decryptedValue = security.decryptData(value)
+                Timber.e(decryptedValue)
+                jsonEncode.decodeFromString(decryptedValue)
+            } else {
+                value as T
+            }
+        }
 
     companion object {
         private val USER_ACCESS_TOKEN = stringPreferencesKey("user_access_token")
